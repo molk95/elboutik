9ab6da1f8fd2f5b217031158fc1ab3ef7597d672
molk95 <molk.saouabi@gmail.com>
2022-05-24 17:43:09 +0200

    add products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-
+import data from './data';
 
 function App() {
   return (
@@ -6,7 +6,27 @@ function App() {
       <header>
         <a href="/">élBoutik</a>
       </header>
-      <main>List des produits</main>
+      <main>
+        <h1>Produits populaires</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price} DT</strong>{' '}
+                </p>
+                <button>Ajouter au panier</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+const data = {
+  products: [
+    {
+      name: 'Bols Feed Me Kitty avec couvercles',
+      slug: 'bols-feed-me-kitty-avec-couvercles',
+      category: 'Articles de maison',
+      image: '/images/img1.jpg', //679px x 829px
+      price: 54,
+      countInStock: 6,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'En porcelaine avec couvercles assortis',
+    },
+    {
+      name: 'Ensemble de sous-verres My Purr-fect',
+      slug: 'ensemble-de-sous-verres-my-purr-fect',
+      category: 'Articles de maison',
+      image: '/images/img2.jpg',
+      price: 75.81,
+      countInStock: 10,
+      rating: 4.1,
+      numReviews: 3,
+      description: 'Laissez les adorables chatons vous saluer dès le matin',
+    },
+    {
+      name: 'Pawsitively Tasse en Céramique Patte de Chat 250ML',
+      slug: 'pawsitively-tasse-en-céramique-patte-de-chat-250ML',
+      category: 'Articles de maison',
+      image: '/images/img3.jpg',
+      price: 82.99,
+      countInStock: 10,
+      rating: 3.7,
+      numReviews: 12,
+      description: 'Cadeau parfait pour les amoureux des chats',
+    },
+    {
+      name: 'Cat & Rose Bucket Bag',
+      slug: 'cat-&-rose-bucket-bags',
+      category: 'Sacs à main',
+      image: '/images/img4.jpg',
+      price: 64,
+      countInStock: 3,
+      rating: 4.7,
+      numReviews: 19,
+      description: "Style sac seau avec beaucoup d'espace",
+    },
+  ],
+};
+export default data;
