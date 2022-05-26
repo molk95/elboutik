8dd167f56dc6779a39e8d9e12ab390d0f4178276
molk95 <molk.saouabi@gmail.com>
2022-05-26 15:03:08 +0200

    bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,33 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import Product from './Product';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
-    <div className="App">
+    <div className="d-flex flex-column site-container">
       <header>
-        <Link to="/">élBoutika</Link>
+        <Navbar bg="danger" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>élBoutika</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
       </header>
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:slug" element={<Product />} />
-        </Routes>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:slug" element={<Product />} />
+          </Routes>
+        </Container>
       </main>
+      <footer>
+      <div className='text-center'> &copy; All right reserved to Molk Saouabi</div>
+      </footer>
     </div>
   );
 }
