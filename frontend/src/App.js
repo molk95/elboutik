989c4fae761d0dd645f2aef4d994f35612fb4aa6
molk95 <molk.saouabi@gmail.com>
2022-06-06 15:39:51 +0200

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -74,7 +74,7 @@ function App() {
       >
         <ToastContainer position="bottom-center" limit={1} />
         <header>
-          <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar className='nav-color' bg="dark" variant="dark" expand="lg">
             <Container>
               <Button
                 variant="dark"
